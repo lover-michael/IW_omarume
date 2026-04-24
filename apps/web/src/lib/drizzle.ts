301166a8f2508a8819be304716f05d020f7a75ee
Doukou Nao <nao.d180513@gmail.com>
2026-04-24 18:04:07 +0900

    update

--- apps/web/src/lib/drizzle.ts
+++ apps/web/src/lib/drizzle.ts
@@ -1,4 +1,12 @@
-﻿import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+﻿import * as schemas from "@repo/database";
+import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(neon(process.env.DATABASE_URL!));
+export const db = drizzle({
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: false,
+  },
+  schema: {
+    ...schemas,
+  },
+});
