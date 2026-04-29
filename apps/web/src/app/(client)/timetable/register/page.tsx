e239cac00d0663908f989c7d3db3c287644c76f0
doukou <nao.d180513@gmail.com>
2026-04-29 14:40:28 +0900

    update

--- apps/web/src/app/(client)/timetable/register/page.tsx
+++ apps/web/src/app/(client)/timetable/register/page.tsx
@@ -178,7 +178,7 @@ export default function PageRegister() {
               </Portal>
             </Combobox.Root>
           </Box>
-          <Box>{stationCollections[0]}</Box>
+          <Box>{stationCollections[0].label}</Box>
         </Box>
       </FormControl>
     </form>
