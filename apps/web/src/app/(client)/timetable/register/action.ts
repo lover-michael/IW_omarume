97b9cb0cce92a903c1174c990989dd7654623ed0
doukou <nao.d180513@gmail.com>
2026-05-15 20:33:22 +0900

    update

--- apps/web/src/app/(client)/timetable/register/action.ts
+++ apps/web/src/app/(client)/timetable/register/action.ts
@@ -76,9 +76,9 @@ export async function SaveTimeTable(props: TimeTableSchemaType) {
 export async function GetStations() {
   const db = getDb();
   const result = await db
-    .selectDistinct({ id: station.id, name: station.name })
+    .selectDistinctOn([station.name])
     .from(station)
-    .orderBy(station.id, station.name);
+    .orderBy(station.name);
 
   const stationNames = result.map((element) => ({
     id: element.id,
