e12e4cff8b335cf36197437c4e2f35d4debfc53d
Doukou Nao <nao.d180513@gmail.com>
2026-06-03 09:25:32 +0900

    update

--- apps/web/src/lib/drizzle.ts
+++ apps/web/src/lib/drizzle.ts
@@ -1,16 +1,13 @@
 ﻿import * as schemas from "@repo/database";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
 // ビルド時に接続することを回避するため遅延評価にしている
 let _db: ReturnType<typeof drizzle>;
 
 export function getDb() {
   if (!_db) {
-    _db = drizzle({
-      connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: false,
-      },
+    _db = drizzle(neon(process.env.DATABASE_URL!), {
       schema: {
         ...schemas,
       },
