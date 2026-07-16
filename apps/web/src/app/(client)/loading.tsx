e0b34e6349d2b55d364c2cf41a3c3f96112ca688
doukou <nao.d180513@gmail.com>
2026-07-16 13:07:54 +0900

    update

--- apps/web/src/app/(client)/loading.tsx
+++ apps/web/src/app/(client)/loading.tsx
@@ -3,16 +3,7 @@ import { Center, Box, Stack, Card } from "@chakra-ui/react";
 export default function Loading() {
   return (
     <Center h="full" w="full" p={"7"}>
-      <Box boxSize={"xl"}>
-        <Stack gapY={"5"}>
-          <Card.Root size={"lg"}>
-            <Card.Body>
-              <Card.Title>
-                <Box textAlign={"center"}></Box>
-              </Card.Title>
-            </Card.Body>
-          </Card.Root>
-        </Stack>
+      <Box height={'xl'} bgColor={'gray.100'}>
       </Box>
     </Center>
   );
