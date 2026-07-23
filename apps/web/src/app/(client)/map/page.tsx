cdb59e4ca54f580e1a2a3465c282d134e13d1dd5
Doukou Nao <nao.d180513@gmail.com>
2026-07-23 11:26:18 +0900

    update page layout

--- apps/web/src/app/(client)/map/page.tsx
+++ apps/web/src/app/(client)/map/page.tsx
@@ -1,17 +1,27 @@
 "use client";
 
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Center, HStack } from "@chakra-ui/react";
 import Image from "next/image";
 
 export default function PageMap() {
   return (
-    <Box h={"100%"} w={"100%"} bgColor={"gray.500"} position={"relative"}>
+    <Box h={"100%"} w={"100%"} position={"relative"}>
       <Image
         src={"/image/KotaniArea_Overall.png"}
         alt={"map"}
-        style={{ width: '80%', height: '80%', position: 'absolute', top: "50%", left: "50%", transform: 'translate(-50%, -50%)' }}
+        style={{ width: '100%', height: '90%', position: 'absolute', top: "50%", left: "50%", transform: 'translate(-50%, -50%)' }}
         fill={true}
       />
+      <Center my={'5'} mx={'5'} py={'5'}>
+        <HStack gap={'5'}>
+          <Button boxShadow={'xl'} bgColor={'green.500'}>
+            バスの現在値
+          </Button>
+          <Button boxShadow={'xl'} bgColor={'green.500'}>
+            ルート確認
+          </Button>
+        </HStack>
+      </Center>
     </Box>
   );
 }
