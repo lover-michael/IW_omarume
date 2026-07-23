c7aaa5add38568dafad7cff0a5b75fcd68319816
Doukou Nao <nao.d180513@gmail.com>
2026-07-23 11:19:49 +0900

    fix CSS

--- apps/web/src/app/(client)/map/page.tsx
+++ apps/web/src/app/(client)/map/page.tsx
@@ -5,11 +5,11 @@ import Image from "next/image";
 
 export default function PageMap() {
   return (
-    <Box h={"100%"} w={"100%"} bgColor={"gray.500"}>
+    <Box h={"100%"} w={"100%"} bgColor={"gray.500"} position={"relative"}>
       <Image
         src={"/image/KotaniArea_Overall.png"}
         alt={"map"}
-        style={{ width: '80%', height: '80%' }}
+        style={{ width: '80%', height: '80%', position: 'absolute', top: "50%", left: "50%", transform: 'translate(-50%, -50%)' }}
         fill={true}
       />
     </Box>
