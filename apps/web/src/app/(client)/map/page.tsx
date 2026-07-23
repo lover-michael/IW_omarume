ad48edc838b166d0a1665d77fa20d2e4b5f15cff
Doukou Nao <nao.d180513@gmail.com>
2026-07-23 10:57:38 +0900

    add Map demo

--- apps/web/src/app/(client)/map/page.tsx
+++ apps/web/src/app/(client)/map/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import { Box } from "@chakra-ui/react";
+import Image from "next/image";
 
 export default function PageMap() {
   return (
     <Box h={"full"} w={"full"} bgColor={"gray.500"}>
-      map
+      <Image
+        src={"/image/KotaniArea_Overall.png"}
+        alt={"map"}
+        width={'80'}
+        height={'80'}
+      />
     </Box>
   );
 }
