0bd5730dfacb1197cbf533d0daf538378148bac1
doukou <nao.d180513@gmail.com>
2026-08-27 22:28:55 +0900

    add delete table function

--- apps/dashboard/src/server/registerTimetable.action.ts
+++ apps/dashboard/src/server/registerTimetable.action.ts
@@ -1,4 +1,5 @@
 import { getDB, station } from "@repo/database"
+import { eq } from "drizzle-orm"
 
 const db = getDB();
 
@@ -6,4 +7,24 @@ type registerProps = Omit<typeof station.$inferSelect, "id" | "create_at">
 
 export const registerTimetable = async (props: registerProps[]) => {
   await db.insert(station).values(props);
+};
+
+type deleteProps = {
+  id: number | null;
+};
+
+export const deleteTimetable = async (props: deleteProps) => {
+  await db.transaction(async (tx) => {
+    const timetableList = await tx.select().from(station);
+    // If the timetable list is empty, do nothing
+    if (timetableList.length === 0) return;
+
+    // If id is null, delete all timetable entries
+    if (props.id === null) {
+      await tx.delete(station);
+      return;
+    }
+
+    await tx.delete(station).where(eq(station.id, props.id));
+  })
 };
